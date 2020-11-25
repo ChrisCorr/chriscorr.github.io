b0dfb5c70a8be4c6bf8a21966d3e33e650cad9b0
Chris <chrizcorr@gmail.com>
2020-11-25 19:30:41 +0100

    Fuck php

--- header.php
+++
@@ -1,23 +0,0 @@
-<!doctype html>
-<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:website="http://ogp.me/ns/website" lang="en-US" itemscope itemtype="http://schema.org/WebPage"  class="no-js" lang="en">
-  <head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Centauri Rising Game</title>
-	<meta name="description" content="Centauri Rising is a story driven Real Time Tactical space combat game for PC.">	<meta property="og:site_name" content="Centauri Rising">	<meta property="og:title" content="Centauri Rising Game">	<meta property="og:description" content="Centauri Rising is a story driven Real Time Tactical space combat game for PC.">	<meta property="og:url" content="http://www.centaurirising.com/">	<meta property="og:image" content="http://www.centaurirising.com/img/centaurirising.jpg">	<meta property="og:type" content="website">	<meta itemprop="name" content="Real Time Tactics Game">	<meta itemprop="url" content="http://www.centaurirising.com/">
-<!--	<link rel="stylesheet" href="css/loader.css" />-->
-<!--	<link rel="stylesheet" href="css/foundation.css" />-->
-<!--	<link rel="stylesheet" href="css/style.css" />-->
-<!--	<link rel="stylesheet" href="css/responsive.css" />-->
-<!--	<link rel="stylesheet" href="css/lity.css" />-->
-<!--	<link rel="stylesheet" href="css/animate.min.css" />-->
-<!--	<link rel="stylesheet" href="css/elegant-icons.css" />-->
-<!--	<link rel="stylesheet" href="css/outline-icons.css" />-->
-<!--	<link rel="stylesheet" href="css/flexslider.css" />-->
-<!--	<link rel="stylesheet" href="css/lightbox.css" />-->
-	<link rel="stylesheet" href="css/loader.css" />	<link rel="stylesheet" href="css/foundation.css" />	<link rel="stylesheet" href="css/style.css" />	<link rel="stylesheet" href="css/responsive.css" />
-	<link rel="icon" type="image/ico" href="img/favicon.ico" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700%7CRaleway:400,700' rel='stylesheet' type='text/css'>
-    </head>
-  <body>
-  
