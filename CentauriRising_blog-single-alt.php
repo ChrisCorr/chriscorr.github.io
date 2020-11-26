6603327b12d4ca9af4bdc7edcb07f57a8d55a846
Chris <chrizcorr@gmail.com>
2020-11-26 21:03:46 +0100

    Blog post
    who we are

---
+++ CentauriRising_blog-single-alt.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:website="http://ogp.me/ns/website" lang="en-US" itemscope itemtype="http://schema.org/WebPage"  class="no-js" lang="en">
+  <head>
+  
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Centauri Rising Game</title>
+	<meta name="description" content="Centauri Rising is a story driven Real Time Tactical space combat game for PC.">
+	<meta property="og:site_name" content="Centauri Rising">
+	<meta property="og:title" content="Centauri Rising Game">
+	<meta property="og:description" content="Centauri Rising is a story driven Real Time Tactical space combat game for PC.">
+	<meta property="og:url" content="http://www.centaurirising.com/">
+	<meta property="og:image" content="http://www.centaurirising.com/img/centaurirising.jpg">
+	<meta property="og:type" content="website">
+	<meta itemprop="name" content="Real Time Tactics Game">
+	<meta itemprop="url" content="http://www.centaurirising.com/">
+	<link rel="stylesheet" href="css/loader.css" />
+	<link rel="stylesheet" href="css/foundation.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/responsive.css" />
+
+	<link rel="icon" type="image/ico" href="img/favicon.ico" />
+	
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700%7CRaleway:400,700' rel='stylesheet' type='text/css'>
+	<!---<script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>-->
+	
+  </head>
+
+  <section style="background-image: linear-gradient(to right, rgba(34, 34, 34, 1),rgba(50, 50, 50, 1),rgba(34, 34, 34, 1)); padding-top:150px;">
+
+		<div class="row">
+			<div class="medium-12 columns">
+				<h1 class="page-title">Battlefield 5 Trailer - Why it's good</h6>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="medium-6 columns">
+				<div class="video-container">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/9OTkhsJUK0U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="medium-6 columns">
+				<p class="lead">
+					Leaving all the controversy around the game aside, the Battlefield V launch trailer does a lot of things right. It’s a great example, among other things, of the tight connection between video editing and music and how that connection results in more than the sum of its parts. After the first lackluster reveal trailer that got an overall negative response, the trailer peeps at DICE returned to form with this bombastic trailer. <strong>It's rare when we rewatch trailers just because of how good they are.</strong> Let’s talk a bit about it.
+				</p>
+			</div>
+
+
+
+		</div>
+
+		</section>
+
+		<section style="background-image: linear-gradient(to right, rgba(34, 34, 34, 1),rgba(50, 50, 50, 1),rgba(34, 34, 34, 1)); padding-top:0px; padding-bottom:25px;">
+
+		<div class="row">
+
+			<div class="medium-12 columns">
+
+				<h3 class="page-title">Structure</h3>
+				<p class="lead">
+						Good trailers get to the point quickly. In the first 20 seconds, this trailer quickly shows off the main selling point of the entire Battlefield franchise; vast arenas where infantry, armor and air forces all interact with each other in fierce battles. Even if you’ve never heard or seen anything related to Battlefield, this trailer shows you exactly what you can expect in less than 20 seconds. If you’ve noticed, this introduction doesn’t show any actual gameplay however, there isn’t a single first person shot in there. That’s because <strong>the hook is emotional.</strong>
+				</p>
+				<p class="lead">
+						Advertising is in big part about selling an idea and on a more deeper level, a feeling. In fact the entire trailer only has 3 short shots of what can constitute actual gameplay, totalling just about 2.5 seconds in length. Clearly the objective of the trailer was to sell the feeling of a battlefield itself.
+				</p>
+
+				<div class="row" style="padding-bottom:10px;">
+					<div class="medium-6 columns">
+						<div class="video-container">
+						<video muted autoplay loop width="560" height="315" >
+						  <source type="video/webm" src="video/B5_Gameplay_1.webm"/>
+							<source type="video/mp4" src="video/B5_Gameplay_1.mp4"/>
+						</video>
+					</div>
+					</div>
+
+					<div class="medium-6 columns">
+						<div class="video-container">
+						<video muted autoplay loop width="560" height="315" >
+						  <source src="video/B5_Gameplay_2.webm" type="video/webm"/>
+							<source src="video/B5_Gameplay_2.mp4" type="video/mp4"/>
+						</video>
+						</div>
+						</div>
+				</div>
+				<p align="center";>
+						The only shots in the entire trailer that could be interpreted as actual gameplay .
+				</p>
+
+				<p class="lead">
+						Good trailers have contrast. High points and low points. This trailer has two distinct break points, driven by the music, that serve as jump off points from which to ramp up intensity. After the above introduction and a short breather, momentum to kick things into high gear is literally shouted at the viewer by a soldier: <i>“Move up, forward!”</i> can be heard during the Awards screen, just before the main section of the trailer shoots off an incredibly well done sequence of shots.
+				</p>
+				<p class="lead">
+						The audience at this point is fully engaged and taking in a well crafted spectacle. Some gameplay related elements can be seen but mostly it's about emotional validation. Seeing familiar sights triggers memories from previous Battlefield games that provided great, exciting moments to the player. This central body of the trailer also has interspaced title screens with quotes from major publications.
+				</p>
+
+				<div class="row" style="padding-bottom:20px;">
+					<div class="medium-4 columns">
+						<img src="img/B5_Title1.png" alt="Title 1" style="padding-bottom:10px;">
+						<p align="center";>
+								Interesting.
+						</p>
+					</div>
+					<div class="medium-4 columns">
+						<img src="img/B5_Title2.png" alt="Title 2" style="padding-bottom:10px;">
+						<p align="center";>
+								About time.
+						</p>
+					</div>
+					<div class="medium-4 columns">
+						<img src="img/B5_Title3.png" alt="Title 3" style="padding-bottom:10px;">
+						<p align="center";>
+								Hell yeah.
+						</p>
+					</div>
+				</div>
+
+
+				<p class="lead">
+						The quotes play an important role. <strong>People who enjoy a game series want to see it succeed</strong>, reading quotes from important publications validates this desire to return to the Battlefield “club”. The trailer crescendos into an explosion and one final quote, the clearest of them: <i>“Best shooter of the year”</i>.
+						<br>	<br>
+						Nuff said.
+						<br>	<br>
+						This is the second breakpoint. The music goes into a slight pause and rises again for one final short bang that caps off the trailer.
+				</p>
+			</div>
+
+		</section>
+
+		<section style="background-image: linear-gradient(to right, rgba(34, 34, 34, 1),rgba(50, 50, 50, 1),rgba(34, 34, 34, 1)); padding-top:0px; padding-bottom:25px;">
+
+		<div class="row">
+
+			<div class="medium-12 columns">
+
+				<h3 class="page-title">Music</h3>
+				<p class="lead">
+				The Battlefield theme carries a lot of emotional strength with fans of the series. The theme was first created for Battlefield 1942, and used again in some shape for each next installment of the franchise, with Battlefield 1 being the only exception.
+				</p>
+				<p class="lead">
+				Expertly using the theme again, this time similar to how the piece was orchestrated for Battlefield 1943 (no PC version for this one!), DICE underpins every single intense moment of arguably the best Battlefield trailer. If you watch the trailer again, try to be conscious about how many beats match what’s happening visually. This naturally happens a lot in most trailers and indeed any sort of serious video editing. Here DICE takes it to the extreme, and instead of ending up feeling overdone, the elegance and production value of the sequences and how they flow into each other make it something else. From impacts and explosions to camera swipes and scene transitions, the music relentlessly pushes the edit forward, mimicking the state of flow during gameplay in one of those matches where everything satisfyingly comes together.
+				</p>
+				<p class="lead">
+				DICE is doing a great job of scripting action sequences to match the music, especially with explosions, there’s 17 instances of impacts and explosions that exactly match a beat in the music, with just a few that don’t. This isn’t to say that this should be a main objective when making a trailer, but in this case it’s a running theme that was successfully pulled off.
+				</p>
+			</div>
+
+		</section>
+
+		<section style="background-image: linear-gradient(to right, rgba(34, 34, 34, 1),rgba(50, 50, 50, 1),rgba(34, 34, 34, 1)); padding-top:0px; padding-bottom:25px;">
+
+		<div class="row">
+
+			<div class="medium-12 columns">
+
+				<h3 class="page-title">Cuts</h3>
+				<p class="lead">
+				The art of film editing is the most significant aspect when producing any kind of video. In fact editing is the only art form that’s unique to film. Cutting techniques are one of the fundamental aspects of editing and one of those techniques is the match cut: It’s a way of cutting between shots by in some way matching what’s visually going on in both. The Battlefield V trailer has some really nice ones, take a look:
+				</p>
+				<div class="row" style="padding-bottom:25px;">
+
+					<div class="medium-6 columns">
+						<div class="video-container">
+						<video width="560" height="315" muted autoplay loop preload="auto">
+							<source src="video/B5_MatchCut_1.webm" type="video/webm"/>
+							<source src="video/B5_MatchCut_1.mp4" type="video/mp4"/>
+						</video>
+					</div>
+					<div class="row">
+									<p align="center";>
+									The soldier is the focal point for both of the cuts.
+								</p>
+					</div>
+					</div>
+
+
+					<div class="medium-6 columns">
+						<div class="video-container">
+						<video width="560" height="315" muted autoplay loop preload="auto">
+						  <source src="video/B5_MatchCut_2.webm" type="video/webm"/>
+							<source src="video/B5_MatchCut_2.mp4" type="video/mp4"/>
+						</video>
+					</div>
+					<div class="row">
+								<p align="center";>
+										Double match cut! Both the fighterplane and the soldier serve as focal points.
+								</p>
+					</div>
+					</div>
+
+				</div>
+
+				<p class="lead">
+				These types of cuts are a way of keeping the action flowing seamlessly while changing scenes completely, which for Battlefield is important to show off the variety of maps. It works to expand the feeling of scale of the game.
+				</p>
+
+			</div>
+
+		</section>
+
+		<section style="background-image: linear-gradient(to right, rgba(34, 34, 34, 1),rgba(50, 50, 50, 1),rgba(34, 34, 34, 1)); padding-top:0px; padding-bottom:25px;">
+
+		<div class="row">
+
+			<div class="medium-12 columns">
+
+				<h3 class="page-title">Why it's good</h3>
+				<p class="lead">
+				The Battlefield V launch trailer is a very polished work of scripting and editing. Its compact and dynamic structure shows off the entire experience of the game in very little time. "Experience" is the key word because there’s next to no actual gameplay in it. This can work when you’re trying to sell an emotion and have the production chops to actually pull it off. The work that went into that production, with very satisfying scripted sequences that cut together perfectly, driven by an epic music piece, is what elevates this trailer beyond just a dull string of explosions.
+				</p>
+
+			</div>
+
+		</section>
+
+		<section class="background-dark-grey">
+			<div class="divider-content">
+				<div class="row">
+					<div class="columns text-center medium-12">
+						<h3 class="text-white page-title">Need a sweet trailer?</h3>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="medium-centered columns text-center push-bottom medium-8">
+						<a class="button button-white" href="index.php#contact">I want one! <i class="icon icon_heart"></i></a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+ <footer class="dark footer-alt">
+			
+			
+				
+				<div class="footer-lower">
+					<div class="row">
+						<div class="medium-7 columns">
+							<span>© Trilemma Interactive All Rights Reserved</span>
+						</div>
+						
+						<div class="medium-5 columns text-right">
+							<a href="#top" class="inner-link button button-small">Top <i class="icon arrow_carrot-up"></i></a>
+						</div>
+					</div>
+				</div>
+			
+			</footer>
+			
+			
+			
+	<script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/smooth-scroll.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/inview.js"></script>
+    <script src="js/skrollr.min.js"></script>
+    <script src="js/scripts.js"></script>
+	<script src="js/lightbox.min.js"></script>
+	<script src="js/spectragram.js"></script>
+	<script src="js/vendor/modernizr.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+  	<link rel="stylesheet" href="css/animate.min.css" />
+	<link rel="stylesheet" href="css/elegant-icons.css" />
+	<link rel="stylesheet" href="css/outline-icons.css" />
+	<link rel="stylesheet" href="css/flexslider.css" />
+	<link rel="stylesheet" href="css/lightbox.css" />
+</html>
